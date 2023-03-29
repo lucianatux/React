1dbcd7766014c7cd6182f4670eb71129010dc888
lucianatux <lucianatux@yahoo.com.ar>
2023-03-29 19:39:45 -0300

    funcion cumple

--- FirstReact/src/App.js
+++ FirstReact/src/App.js
@@ -7,9 +7,22 @@ function Saludo(moment, name){
   return saludo;
 }
 
+function Cumple(day, month){
+  let cumple = (
+  <div>
+    <h4>"Quiero un regalo,</h4>
+    <p>mi cumple es el {day} de {month}"</p>
+  </div>
+  );
+
+  return cumple;
+}
+
 
 function App() {
   let nombre = 'Luciana';
+  let dia = 21;
+  let mes = 'Noviembre';
   let momento = 'morning';
   let presentacion = <h2>"Hola, soy {nombre}"</h2>;
 
@@ -24,6 +37,7 @@ function App() {
         {alert('Hola Mundo')}
         {presentacion}
         {Saludo(momento, nombre)}
+        {Cumple(dia, mes)}
         <a
           className="App-link"
           href="https://reactjs.org"

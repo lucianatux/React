8df1bf8dc300c87626d6dd53f07fb1d2ff8aab99
lucianatux <lucianatux@yahoo.com.ar>
2023-03-30 15:28:35 -0300

    state

--- FirstReact/src/components/SegundoComponente.js
+++ FirstReact/src/components/SegundoComponente.js
@@ -1,7 +1,12 @@
 import React, {Component} from 'react';
 
 class SegundoComponente extends Component {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            image: 'https://live.staticflickr.com/65535/52477609893_441ea3a0d5_w.jpg'
+        };
+    }
     render() {
         const {title, description} = this.props;
         return (
@@ -10,6 +15,7 @@ class SegundoComponente extends Component {
                 <h4>Estoy probando</h4>
                 <h2>{title}</h2>
                 <p>{description}</p>
+                <img src={this.state.image}/>
                 <hr />
             </React.Fragment>
         );

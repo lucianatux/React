1ccb0a3944702dce873f346d6895cc0a49cfb9a8
lucianatux <lucianatux@yahoo.com.ar>
2023-04-04 21:46:01 -0300

    Create Exercises.js

---
+++ FirstReact/src/pages/Exercises.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import PrimerComponente from '../components/PrimerComponente';
+import SegundoComponente from '../components/SegundoComponente';
+
+class Exercises extends React.Component {
+
+}
+
+export default Exercises;

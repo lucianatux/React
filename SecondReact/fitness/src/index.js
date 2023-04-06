97cc2b22884e909b830f85266cb3480b030876a8
lucianatux <lucianatux@yahoo.com.ar>
2023-04-05 22:01:04 -0300

    react jsx

--- SecondReact/fitness/src/index.js
+++ SecondReact/fitness/src/index.js
@@ -6,7 +6,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = <h1>Hello World</h1>;
+function getName(user){
+  return `${user.firstname} ${user.lastname}`;
+}
+
+function getGreeting(user){
+  if(user){
+    return <h1>Hello {getName(user)}</h1>
+  }
+  return <h1>Hello Stranger</h1>
+}
+
+const user = {
+  firstname: 'Joan',
+  lastname: 'Doe',
+  avatar: 'https://live.staticflickr.com/65535/52477609893_441ea3a0d5_w.jpg'
+}
+//const name = 'Lu';
+//const element = <h1>Hello {name}</h1>;
+//const element = <h1>Hello {getName(user)}</h1>;
+const element = (<div>
+                  {getGreeting(user)} 
+                  <img src={user.avatar} alt='avatar'/>
+                </div>)
 const container = document.getElementById('root');
 
 //ReactDOM.render(que, donde)

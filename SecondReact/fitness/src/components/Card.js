d0fa5559e629ecd31d23f67f94cbbf5a3e6b115b
lucianatux <lucianatux@yahoo.com.ar>
2023-04-10 21:14:35 -0300

    setState

--- SecondReact/fitness/src/components/Card.js
+++ SecondReact/fitness/src/components/Card.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import penguin from '../images/pinguino.jpg';
+//import penguin from '../images/pinguino.jpg';
 import ladrillos from '../images/ladrillos.jpg';
 import './styles/Card.css';
 
@@ -12,7 +12,13 @@ class Card extends React.Component {
             image:'https://live.staticflickr.com/65535/52477063536_e04770fe87_z.jpg'
         };
     }
-    
+    componentDidMount(){
+        setTimeout(() => {
+            this.setState({
+                image:'https://live.staticflickr.com/65535/52477609923_92cdb7df0e_z.jpg'
+            })
+        }, 5000)
+    }
     render() {
         return (
             <div className='card mx-auto fitness-card'
